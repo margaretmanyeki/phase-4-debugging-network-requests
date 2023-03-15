69e964300b832e71d1ab6f944bdabaafdd6c55a2
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-15 19:16:53 +0300

    added sln

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -29,6 +29,22 @@ function MovieForm() {
       });
   }
 
+
+function handleSubmit(e) {
+  e.preventDefault();
+  fetch("/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
   function handleChange(e) {
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
