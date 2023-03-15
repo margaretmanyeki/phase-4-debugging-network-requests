33801343bf0c1f1c909d9757cf61bec43862d24f
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-15 19:24:49 +0300

    added sln

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,9 @@
 class MoviesController < ApplicationController
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
   def create
     movie = Movie.create(movie_params)
     render json: movie, status: :created
@@ -8,11 +13,6 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
-  end
-
-  def index
-    movies = Movie.all
-    render json: movies
   end
 
 end
